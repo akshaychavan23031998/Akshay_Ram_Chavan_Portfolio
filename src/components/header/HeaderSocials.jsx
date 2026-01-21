f081e3d4f65ba4404ebc5d390c50fc069ea9a84b
Akshay Ram Chavan <111384131+akshaychavan23031998@users.noreply.github.com>
2026-01-21 11:14:09 +0530

    resolved the build failure

--- src/components/header/HeaderSocials.jsx
+++ src/components/header/HeaderSocials.jsx
@@ -1,16 +1,38 @@
-import React from 'react'
-import { FaLinkedin } from 'react-icons/fa'
-import { FaGithub } from "react-icons/fa6";
-import { FaSquareXTwitter } from "react-icons/fa6";
+import React from "react";
+import { FaLinkedin } from "react-icons/fa";
+import { FaGithub, FaSquareXTwitter } from "react-icons/fa6";
 
 const HeaderSocials = () => {
   return (
-    <div className='header_socials'>
-        <a href='https://www.linkedin.com/in/akshay-chavan23/' target='_blank'><FaLinkedin /></a>
-        <a href='https://github.com/akshaychavan23031998' target='_blank'><FaGithub /></a>
-        <a href='https://twitter.com/AkshayC23480775' target='_blank'><FaSquareXTwitter /></a>
-    </div>
-  )
-}
+    <div className="header_socials">
+      <a
+        href="https://www.linkedin.com/in/akshay-chavan23/"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="LinkedIn"
+      >
+        <FaLinkedin />
+      </a>
 
-export default HeaderSocials
+      <a
+        href="https://github.com/akshaychavan23031998"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="GitHub"
+      >
+        <FaGithub />
+      </a>
+
+      <a
+        href="https://twitter.com/AkshayC23480775"
+        target="_blank"
+        rel="noreferrer"
+        aria-label="Twitter"
+      >
+        <FaSquareXTwitter />
+      </a>
+    </div>
+  );
+};
+
+export default HeaderSocials;
